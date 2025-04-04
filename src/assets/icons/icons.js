c0b968d83e4c5e737e4b92e5d7ae6c8b50a30eba
John Gathaika <johngathiakaw@gmail.com>
2025-04-04 09:24:02 +0300

    svg icons and export js

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,25 @@
+import moreIcon from './more.svg';
+import moreIconBg from './more2.svg';
+import addIconBg from'./add-icon.svg';
+import addIcon from'./add-icon2.svg';
+import nextIcon from './next-icon.svg';
+import playIcon from './play-icon.svg';
+import playState from './play-state-top.svg';
+import prevIcon from './prev-icon.svg';
+import returnIcon from './return.svg';
+import returnIconBg from './return2.svg';
+import logo from './logo.svg'
+
+export {
+    moreIcon,
+    moreIconBg,
+    addIcon,
+    addIconBg,
+    nextIcon,
+    playIcon,
+    playState,
+    prevIcon,
+    returnIcon,
+    returnIconBg,
+    logo
+}
