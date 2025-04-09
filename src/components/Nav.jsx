44ed6827edfe96955cad4ef8ef7eb14f4e8efacc
John Gathaika <johngathiakaw@gmail.com>
2025-04-09 03:12:17 +0300

    clik simulation input element

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,22 @@
-// import { addIcon } from "../assets/icons/icons";
+import { addIconBg,moreIcon } from "../assets/icons/icons";
 const Nav = ({ onPlaylistUpdate  }) => {
     const collectSongs = (event) => {
         const selectedSongs = Array.from(event.target.files);
         onPlaylistUpdate(selectedSongs);
     }
 
+    const simulateAdd = () => {
+        const input = document.querySelector('input[type="file"]');
+
+        input.click();
+    }
+
+
 
     return(
         <nav className="nav-elements" >
+            <img src={moreIcon} alt="" />
+            <div className="input" >
             <input 
                 type="file"
                 webkitdirectory="true"
@@ -15,6 +24,8 @@ const Nav = ({ onPlaylistUpdate  }) => {
                 multiple
                 autoFocus
             />
+            <img src={addIconBg} alt="add-icon" onClick={simulateAdd} />
+            </div>
         </nav>
     )
 }
